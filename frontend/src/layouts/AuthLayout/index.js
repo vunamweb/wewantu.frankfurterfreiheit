a5fc49f6f6ef5c8ef2a76f65df4a8c18d01bd2bf
thuy86 <thuthuyqn12@gmail.com>
2024-06-28 16:20:29 +0700

    improve code
    get list user from backend

--- frontend/src/layouts/AuthLayout/index.js
+++ frontend/src/layouts/AuthLayout/index.js
@@ -13,6 +13,7 @@ import functions from '../../function/function';
 import { APIClient } from '../../helpers/apiClient';
 import { getLoggedInUser } from '../../helpers/authUtils';
 import { SET_LIST_USER_PROFILE } from '../../redux/layout/constants';
+import config from "../../config";
 
 class Index extends Component {
     constructor(props) {
@@ -27,6 +28,7 @@ class Index extends Component {
     };
 
     async componentDidMount() {
+        const urlListApplicant = config.API_URL + "list_applicant";
         var getLayoutMode = localStorage.getItem("layoutMode");
         this.props.changeLayoutMode(getLayoutMode);
         if (getLayoutMode) {
@@ -36,72 +38,22 @@ class Index extends Component {
         }
 
         const admin = getLoggedInUser()[0];
+
+        let data = {};
+        data.user_id = admin.user_id;
+
         let companylist = await new APIClient().get('companylist');
         let addresslist = await new APIClient().get('addresslist');
-        let searchJob = await new APIClient().get('list_job_search_profiles');
-        let allUserDriveList = await new APIClient().get('all/user_driver');
-        let allUserLanguageList = await new APIClient().get('all/user_language');
-        let jobList = await new APIClient().get('user/' + admin.user_id + '/job_search_profiles');
-        let watchlist = await new APIClient().get('user/' + admin.user_id + '/user_watchlist');
-        this.renData(addresslist, searchJob, companylist, allUserDriveList, allUserLanguageList, jobList);
-
-        // let currentage = this.capitalizeFirstLetter(this.props.router.location.pathname);
-
-        //set document title according to page path name
-        // document.title = currentage + " | Chatvia - Responsive Bootstrap 5 Admin Dashboard";
+        new APIClient().create(urlListApplicant, data).then(searchJob => {
+            if (searchJob) {
+                this.renData(addresslist, searchJob, companylist);
+            }
+        })
     }
 
-    renData(addresslist, searchJob, companylist, allUserDriveList, allUserLanguageList, jobList) {
+    renData(addresslist, searchJob, companylist) {
 
         let newsearchJob = [...searchJob]
-        const jobSearchIds = jobList.map(job => job.job_search_profile_id);
-
-        // watchlist.length > 0 && watchlist.map(itemwl => {
-
-        //     newsearchJob = newsearchJob.filter(item => item.user.user_id !== itemwl.user_add_id && itemwl.type == 1)
-        // })
-
-
-        // get list of user from web
-        newsearchJob = newsearchJob.filter(item => {
-            return item.user.firebase_token != null
-        })
-
-        // set drive license for user
-        newsearchJob.length > 0 && newsearchJob.map(job => {
-            try {
-                job.user.drive = [];
-
-                allUserDriveList.length > 0 && allUserDriveList.map(drive => {
-                    if (job.user.user_id == drive.user_id)
-                        job.user.drive.push(drive.driver_license_id)
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        })
-
-        // set language for user
-        newsearchJob.length > 0 && newsearchJob.map(job => {
-            try {
-                job.user.language = {};
-                job.user.language.mother = [];
-                job.user.language.foreign = [];
-
-                allUserLanguageList.length > 0 && allUserLanguageList.map(language => {
-                    if (job.user.user_id == language.user_id) {
-                        if (language.level == 0)
-                            job.user.language.mother.push(language.language_id)
-                        else
-                            job.user.language.foreign.push(language.language_id)
-                    }
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        })
-
-        newsearchJob = functions.mixJobProfile(newsearchJob);
 
         newsearchJob.map((serJob, index) => {
             let addr = {};
@@ -113,36 +65,11 @@ class Index extends Component {
             company = companylist !== null && companylist.filter(item => item.company_id !== null && item.company_id.includes(serJob.user.company_id))
             if (company.length > 0)
                 newsearchJob[index].company = company;
+        });
 
 
 
-            /*
-            let checkinwatchlist = watchlist.filter(item => item.job_search_profile.job_search_profile_id.includes(serJob.job_search_profile_id)).length >0
-            if(checkinwatchlist){
-                try {
-                  //  newsearchJob[index] !== null && newsearchJob.splice(0,1);
-                   console.log(newsearchJob[index])
-                } catch (e) {
-                    throw new Error('No neighbour found.');
-                }
-            }
-            */
-
-        }); 
-
-       
-
-       this.props.setListUserProfile(newsearchJob);
-
-        // localStorage.setItem('listUserProfile', JSON.stringify(newsearchJob));
-
-        // this.setState({
-        //     // listuser: listuser,
-        //     listJobProfileAll: searchJob,
-        //     listJobProfileMobile: newsearchJob.filter(val => val.user.user_id !== admin.user_id),
-        //     jobList: jobList,
-        //     //loading: true
-        // })
+        this.props.setListUserProfile(newsearchJob);
     }
 
     render() {
@@ -177,4 +104,4 @@ const mapStateToProps = state => {
 //     setListUserProfile: () => dispatch({ type: SET_LIST_USER_PROFILE }),
 // });
 
-export default withRouter(connect(mapStateToProps,{ changeLayoutMode,setListUserProfile })(Index))
+export default withRouter(connect(mapStateToProps, { changeLayoutMode, setListUserProfile })(Index))
